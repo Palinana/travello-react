12c66efbeb2755b9a66a4c37af12d71445aebe14
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-12-03 11:06:02 -0500

    First commit

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -3,6 +3,7 @@ import HotelDetails from '../layout/HotelDetails';
 import Sidebar from '../layout/Sidebar';
 
 //For now renders HotelDetails instead of Hotels list
+
 export default () => {
   return (
     <div className="content">
